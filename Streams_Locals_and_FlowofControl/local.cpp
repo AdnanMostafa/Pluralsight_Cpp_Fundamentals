02adc154651950edb4cb0e93c784f0b1316643ac
Adnan Mostafa <adnansakibinternships@gmail.com>
2020-07-07 21:17:52 +0000

    Completed locals and flow of control modules

---
+++ Streams_Locals_and_FlowofControl/local.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n" << 1.5 + 2.75;
+
+  cout << endl << endl;
+
+  // int i= 4.9;
+  // cout << i << endl;
+
+  auto j= 5;
+  cout << j << endl;
+
+  auto f= 4.9;
+  j=f;
+  cout << f << endl;
+
+  // float g= 9.0/2;
+  // cout << g << endl;
+
+  //type needs to agree
+  //i= "hello";
+  //f= i/"hello";
+  return 0;
+}
